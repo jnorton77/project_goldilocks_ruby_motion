5531af2a495177d75ee41c22daa7f666d0c4daa7
Dan-and-John <dbrown14@gmail.com>
2014-04-09 17:43:49 -0500

    add loadView method - changed name of XIB file to match

--- goldilocks/app/goldilocks_view_controller.rb
+++ goldilocks/app/goldilocks_view_controller.rb
@@ -1,1 +1,8 @@
 class GoldilocksViewController < UIViewController
+
+  def loadView
+    views = NSBundle.mainBundle.loadNibNamed "interface", owner:self, options:nil
+    self.view = views[0]
+    @counter = 0
+    @view_handle = self.view
+  end
