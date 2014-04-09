630d929c44e7f8b8e7d710b43fbb0b0a27cd3d23
Dan-and-John <dbrown14@gmail.com>
2014-04-09 17:53:19 -0500

    add viewDidLoad method

--- goldilocks/app/goldilocks_view_controller.rb
+++ goldilocks/app/goldilocks_view_controller.rb
@@ -6,3 +6,17 @@ class GoldilocksViewController < UIViewController
     @counter = 0
     @view_handle = self.view
   end
+
+  def viewDidLoad
+    panic_button = view.viewWithTag 5
+    panic_edge_button = view.viewWithTag 4
+    learning_button = view.viewWithTag 3
+    comfort_edge_button = view.viewWithTag 2
+    comfort_button = view.viewWithTag 1
+
+    panic_button.addTarget(self, action:'panicTapped:', forControlEvents:UIControlEventTouchUpInside)
+    panic_edge_button.addTarget(self, action:'panicEdgeTapped:', forControlEvents:UIControlEventTouchUpInside)
+    learning_button.addTarget(self, action:'learningTapped:', forControlEvents:UIControlEventTouchUpInside)
+    comfort_edge_button.addTarget(self, action:'comfortEdgeTapped:', forControlEvents:UIControlEventTouchUpInside)
+    comfort_button.addTarget(self, action:'comfortTapped:', forControlEvents:UIControlEventTouchUpInside)
+  end
